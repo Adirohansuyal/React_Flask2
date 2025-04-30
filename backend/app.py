ada3abc8fc51cce9b5f8b624d5e9da620a3cb381
ohanSuyal <144371692+Adirohansuyal@users.noreply.github.com>
2025-04-30 19:09:16 +0530

    new

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,22 @@
-# app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import numpy as np
 
-app = Flask(__name__)
-CORS(app)
-
 model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    features = np.array([[data['area'], data['bedrooms'], data['bathrooms']]])
-    prediction = model.predict(features)
-    return jsonify({'predicted_price': round(prediction[0], 2)})
+    data = request.get_json()
+    area = data['area']
+    bedrooms = data['bedrooms']
+    bathrooms = data['bathrooms']
+    features = np.array([[area, bedrooms, bathrooms]])
+    predicted_price = model.predict(features)[0]
+    return jsonify({'predicted_price': round(predicted_price, 2)})
 
 if __name__ == '__main__':
     app.run(debug=True)
